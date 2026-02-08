74364a02e4df5150620ffed1388e31de50307592
Melih Çetinkaya <melih07160716@gmail.com>
2026-02-08 11:45:10 +0300

    last changes

--- app/src/main/java/com/example/irrigationfrontend/instance/RetrofitInstance.kt
+++ app/src/main/java/com/example/irrigationfrontend/instance/RetrofitInstance.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
 
-    private const val BASE_URL = "http://10.39.192.215:8085/"
+    private const val BASE_URL = "http://10.60.221.152:8085/"
 
 
     val retrofit by lazy {
